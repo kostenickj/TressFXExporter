c0e9163b60be4b58937699385ca94d5dcc547145
Jacob Kostenick <kostenickj@gmail.com>
2019-02-25 02:50:21 -0800

    all options there

--- TFXExport.py
+++ TFXExport.py
@@ -14,7 +14,7 @@ import random
 import sys
 import bpy
 import json
-
+from bpy_extras.io_utils import ExportHelper
 #__________________________________________________________________________
 
 def OnTressFXBaseMeshChange(self, context):
@@ -46,9 +46,65 @@ class FTressFXProps(bpy.types.PropertyGroup):
     def register(FTressFXProps):
         FTressFXProps.sBaseMesh = bpy.props.StringProperty(
             name="Base Mesh", 
-            description="The mesh the hairs are attached to",
+            description="The mesh the hairs are attached and weighted to",
             update=OnTressFXBaseMeshChange
             )
+
+        FTressFXProps.eNumVertsPerStrand = bpy.props.EnumProperty(
+            name='Num Verts Per Strand',
+            description='Number of vertices per strand',
+            items=[('4', '4', '4'),('8', '8', '8'),('16', '16', '16'),('32', '32', '32')]
+            )
+
+        FTressFXProps.fMinimumCurveLength = bpy.props.FloatProperty(
+            name='',
+            description='Minimum curve length is to filter out hair shorter than the input length. In some case, it is hard to get rid of short hair using modeling tool. This option will be handy in that case. If it is set to zero, then there will be no filtering. ',
+            min = 0,
+            soft_min = 0,
+            precision = 6
+            )
+
+        FTressFXProps.bBothEndsImmoveable = bpy.props.BoolProperty(
+            name="Both ends immovable", 
+            description="makes both ending vertices get zero inverse mass",
+            default=False
+            )
+
+        FTressFXProps.bInvertZAxis = bpy.props.BoolProperty(
+            name="Invert Z-axis of Hairs", 
+            description="inverts the Z component of hair vertices. This may be useful to deal with some engines using left-handed coordinate system",
+            default=False
+            )
+        
+        FTressFXProps.bInvertYAxisUV = bpy.props.BoolProperty(
+            name="Invert Y-axis of UV coordinates", 
+            description="inverts Y component of UV coordinates",
+            default=False
+            )
+
+        FTressFXProps.bRandomizeStrandsForLOD = bpy.props.BoolProperty(
+            name="Randomize strands for LOD", 
+            description="Randomizes hair strand indices so that LOD can uniformly reduce hair strands",
+            default=True
+            )
+
+        FTressFXProps.bExportTFX = bpy.props.BoolProperty(
+            name="Export .tfx File", 
+            description="Exports the hair geometry and vertice data",
+            default=True
+            )
+
+        FTressFXProps.bExportTFXBone = bpy.props.BoolProperty(
+            name="Export .tfxbone File", 
+            description="Export bone animation data for the skin mesh, requires base mesh to be set",
+            default=False
+            )
+
+        FTressFXProps.sOutputDir = bpy.props.StringProperty(
+            name="Export Directory", 
+            description="The export directory",
+            )
+
         bpy.types.Object.TressFXProps = bpy.props.PointerProperty(
             type=FTressFXProps, 
             name="TressFX Properties", 
@@ -81,27 +137,108 @@ class FTressFXPanel(bpy.types.Panel):
         oTFXProps = oTargetObject.TressFXProps
 
         MainBox = layout.box()
-        MainBox.label(text='TressFX')
+        MainBox.label(text='Export Settings')
 
 
         if oTargetObject is not None:
 
+            #base mesh selection
             BaseMeshRow = MainBox.row()
-            BaseMeshBox = BaseMeshRow.box()
-            split = BaseMeshBox.split(percentage=0.4)            
-            LeftCol = split.column()
+            BaseMeshSplit = BaseMeshRow.split(percentage=0.5)            
+            LeftCol = BaseMeshSplit.column()
             LeftCol.label(text="Base Mesh: ")
-            RightCol = split.column()
+            RightCol = BaseMeshSplit.column()
             RightCol.prop_search(oTFXProps, "sBaseMesh",  context.scene, "objects", text="")
 
+            #num verts selection
+            NumVertsPerStrandRow = MainBox.row()
+            NumVertsSplit = NumVertsPerStrandRow.split(percentage=0.5)
+            LeftCol = NumVertsSplit.column()
+            LeftCol.label(text="Num Verts Per Strand:")
+            RightCol = NumVertsSplit.column()
+            RightCol.prop(oTFXProps, "eNumVertsPerStrand", text="")
+
+            #minimum curve length
+            MinLengthRow = MainBox.row()
+            MinLengthSplit = MinLengthRow.split(percentage=0.5)
+            LeftCol = MinLengthSplit.column()
+            LeftCol.label(text="Min Curve Length:")
+            RightCol = MinLengthSplit.column()
+            RightCol.prop(oTFXProps, "fMinimumCurveLength", text="")
+
+            #Both Ends Immovable
+            BothEndsRow = MainBox.row()
+            BothEndsSplit = BothEndsRow.split(percentage=0.5)
+            LeftCol = BothEndsSplit.column()
+            LeftCol.label(text="Both Ends Immovable:")
+            RightCol = BothEndsSplit.column()
+            RightCol.prop(oTFXProps, "bBothEndsImmoveable", text="")
+
+            #Invert z
+            InvertZRow = MainBox.row()
+            InvertZSplit = InvertZRow.split(percentage=0.5)
+            LeftCol = InvertZSplit.column()
+            LeftCol.label(text="Invert Z-axis of hairs:")
+            RightCol = InvertZSplit.column()
+            RightCol.prop(oTFXProps, "bInvertZAxis", text="")
+
+            #Invert Y uv
+            InvertYUVRow = MainBox.row()
+            InvertYUVSplit = InvertYUVRow.split(percentage=0.5)
+            LeftCol = InvertYUVSplit.column()
+            LeftCol.label(text="Invert Y-axis of UVs:")
+            RightCol = InvertYUVSplit.column()
+            RightCol.prop(oTFXProps, "bInvertYAxisUV", text="")
+
+            #randomize strands for lod
+            RandomizeStrandsRow = MainBox.row()
+            RandomizeStrandsSplit = RandomizeStrandsRow.split(percentage=0.5)
+            LeftCol = RandomizeStrandsSplit.column()
+            LeftCol.label(text="Randomize Strands For LOD:")
+            RightCol = RandomizeStrandsSplit.column()
+            RightCol.prop(oTFXProps, "bRandomizeStrandsForLOD", text="")
+
+            #export tfx
+            ExportTFXRow = MainBox.row()
+            ExportTFXSplit = ExportTFXRow.split(percentage=0.5)
+            LeftCol = ExportTFXSplit.column()
+            LeftCol.label(text="Export Hair Data (.tfx):")
+            RightCol = ExportTFXSplit.column()
+            RightCol.prop(oTFXProps, "bExportTFX", text="")
+
+            #export tfxbone
+            ExportTFXBoneRow = MainBox.row()
+            ExportTFXBoneSplit = ExportTFXBoneRow.split(percentage=0.5)
+            LeftCol = ExportTFXBoneSplit.column()
+            LeftCol.label(text="Export Bone Data (.tfxbone):")
+            RightCol = ExportTFXBoneSplit.column()
+            RightCol.prop(oTFXProps, "bExportTFXBone", text="")
+
+            #export path label
+            OutputPathRow = MainBox.row()
+            OutputPathRow.label(text="Output Path:")
+
+            #export path picker and value
+            OutPathPickerRow = MainBox.row()
+            OutPathPickerRow.prop(oTFXProps, 'sOutputDir', text='')            
+            OutPathPickerRow.operator("tressfx.export_dir", icon="FILE_FOLDER", text="")
+
+            ExportRow = MainBox.row()             
+            ExportRow.operator("tressfx.export", text="Export")
+
+        
 
 
-class TRESSFX_SomeOperatorTodo(bpy.types.Operator):
+
+
+class FTressFXExport(bpy.types.Operator):
     '''
     TODO
     '''    
-    bl_idname = "TressFX.todo"
-    bl_label = "TressFX: todo"
+
+    #NOTE bl_idname has to be all lowercase :(
+    bl_idname = "tressfx.export"
+    bl_label = "TressFX: Export"
 
     @classmethod
     def poll(cls, context):
@@ -111,22 +248,52 @@ class TRESSFX_SomeOperatorTodo(bpy.types.Operator):
 
 
     def execute(self, context):
-        
+        print("todo")
         oTargetObject = context.active_object
         return {'FINISHED'}      
 
 
-    
+class FDirectorySelector(bpy.types.Operator, ExportHelper):
+    bl_idname = "tressfx.export_dir"
+    bl_label = "Ok"
+    filename_ext = ""
+    filter_folder = bpy.props.BoolProperty(default=True, options={'HIDDEN'})
+    bl_options = {'REGISTER'}
+
+    # Define this to tell 'fileselect_add' that we want a directoy
+    directory = bpy.props.StringProperty(
+        name="Outdir Path",
+        description="Where I will save my stuff"
+        # subtype='DIR_PATH' is not needed to specify the selection mode.
+        # But this will be anyway a directory path.
+        )
+
+    def execute(self, context):
+        print("Selected dir: '" + self.directory + "'")
+        context.active_object.TressFXProps.sOutputDir = self.directory
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        # Open browser, take reference to 'self' read the path to selected
+        # file, put path in predetermined self fields.
+        # See: https://docs.blender.org/api/current/bpy.types.WindowManager.html#bpy.types.WindowManager.fileselect_add
+        context.window_manager.fileselect_add(self)
+        # Tells Blender to hang on for the slow user input
+        return {'RUNNING_MODAL'}
 
 
 def register():
+    bpy.utils.register_class(FDirectorySelector)
+    bpy.utils.register_class(FTressFXExport)
     bpy.utils.register_class(FTressFXPanel)
     bpy.utils.register_class(FTressFXProps)
 
-
 def unregister():
+    bpy.utils.unregister_class(FDirectorySelector)
+    bpy.utils.unregister_class(FTressFXExport)
     bpy.utils.unregister_class(FTressFXPanel)
     bpy.utils.unregister_class(FTressFXProps)
+
 
 if __name__ == "__main__":
     register()
